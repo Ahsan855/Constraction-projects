71c6add02b30a84d5bfc3d7944498ee48895f076
Ahsan855e <a.r.ahsanhabib08@gmail.com>
2022-05-29 12:21:32 +0600

    done contact page

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-20 w-10/12 mx-auto">
+        <div className="h-3/4 my-auto">
+          <h1 className="text-2xl hover:text-primary mb-10">
+            Contact Information
+          </h1>
+          <p className="hover:text-primary mb-5">
+            Stock Building, <br /> 125 Main Street 1st Lane, <br /> San
+            Francisco, USA
+          </p>
+          <p className="hover:text-primary mb-5">
+            (001)7777 377 977 <br /> (001) 8888 569 8888
+          </p>
+          <p className="hover:text-primary">muhammadrifathasan9869@gmail.com</p>
+        </div>
+        <div>
+          <h1 className="text-2xl hover:text-primary">Leave Your Message</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            class="input input-bordered input-primary w-full max-w-xs mt-7"
+          />
+          <input
+            type="text"
+            placeholder="Your Email"
+            class="input input-bordered input-primary w-full max-w-xs my-8"
+          />{" "}
+          <br />
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered input-primary w-full max-w-xs mb-4"
+          />
+          <br />
+          <textarea
+            class="textarea input-bordered input-primary mb-5"
+            placeholder="Your Message"
+          ></textarea>{" "}
+          <br />
+          <button className="uppercase btn btn-primary">Send message</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
